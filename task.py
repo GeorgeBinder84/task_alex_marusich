d710d189bc45514629e892b692b19f8e9fda0d8f
GeorgeBinder84 <bigbinder84@gmail.com>
2022-11-23 21:18:20 +0100

    renamed 'get_info_hrefs' to 'get_https_methods' separated 'get_https_methods' into subroutines

--- task.py
+++ task.py
@@ -1,64 +1,84 @@
-'''
-1) Получает на вход произвольное множество строк.														+
-2) Итерируется по этим строкам и определяет, является ли эта строка ссылкой или нет.					+
-3) Если эта строка не ссылка, выводится уведомление: Строка "X" не является ссылкой.					+
-4) Если является ссылкой, то
-	1) Приложение должно определить какие методы доступны по этой ссылки
-		1) Проверяются все http методы.																	+/- кроме 'CONNECT','TRACE'
-		2) Доступным считается метод, обработка которого завершилась не 405 ошибкой.					+
-	3) Передаваемые данные и ошибки от сервера не важны.												+
-	4) Выполнив запрос приложение сохраняет код ответа.													+
-6) Результатом работы приложением будет словарь, состоящий из ссылок и информации о доступных метода.	+
-'''
-
 import json
 import requests
 import re
+def get_https_methods(array_urls):
+    methods = ['GET', 'POST', 'PUT', 'HEAD', 'DELETE', 'PATCH', 'OPTIONS', 'CONNECT', 'TRACE']
+    def run_http_methods(str_url, str_method):
 
-def get_info_hrefs(strings):
-    if hasattr(strings, '__iter__') and type(strings) != str:
-        strings = set(strings)
-    else:
-        strings = {strings,}
-
-    session = requests.Session()
-    result = dict()
-    URL_REGEX = re.compile(r"^(?:http(s)?:\/\/)[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+$")
-
-    for i_string in strings:
-        #i_string is not None and
-        if re.match(URL_REGEX, str(i_string)) is None:
-            print(f"Строка '{i_string}' не является ссылкой.")
-            continue
         try:
-            result_methonds = dict()
-            status = session.get(i_string).status_code
-            if status == 405: continue
-            result_methonds['GET'] = status
-
-            status = session.post(i_string).status_code
-            if status != 405: result_methonds['POST'] = status
-
-            status = session.put(i_string).status_code
-            if status != 405: result_methonds['PUT'] = status
-
-            status = session.head(i_string).status_code
-            if status != 405: result_methonds['HEAD'] = status
-
-            status = session.delete(i_string).status_code
-            if status != 405: result_methonds['DELETE'] = status
-
-            status = session.patch(i_string).status_code
-            if status != 405: result_methonds['PATCH'] = status
-
-            status = session.options(i_string).status_code
-            if status != 405: result_methonds['OPTIONS'] = status
-
-            result[i_string] = result_methonds
-
+            session = requests.Session()
+            ref_method = getattr(session, str_method.lower())
+            status = ref_method(str_url).status_code
+            return status
         except (requests.exceptions.RequestException):
-            pass
+            return 405
+        except AttributeError:
+            return 405
 
-    with open("task.json", "w") as outfile:
-        json.dump(result, outfile)
-    return result
+    URL_REGEX = re.compile(r"^(?:http(s)?:\/\/)[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+$")
+    def check_is_url(str_url):
+        if type(str_url) == str and  re.match(URL_REGEX, str_url):
+            return True
+        else:
+            print(f"Строка '{str_url}' не является ссылкой.")
+            return False
+    def create_set(arr):
+        if hasattr(arr, '__iter__') and type(arr) != str:
+            arr = set(arr)
+        else:
+            arr = {arr,}
+        return arr
+
+    def  delete_items_there_are_not_urls(arr):
+        result = list()
+        for item in arr:
+            if check_is_url(item) == True:
+                result.append(item)
+        return set(result)
+
+    def create_structure(array_urls):
+        result = {}
+        for url in array_urls:
+            result[url] = {}
+            for method in methods:
+                result[url][method] = 405
+
+        return result
+
+    def clean_structure(array_urls):
+        for url in array_urls:
+            for method in methods:
+                if array_urls[url][method] == 405:
+                    del array_urls[url][method]
+            if array_urls[url] == {}:
+                del array_urls[url]
+
+        return array_urls
+
+    def  create_and_write_to_file(arr):
+        with open("task.json", "w") as outfile:
+            json.dump(arr, outfile)
+
+    def main():
+        for i_url in array_urls:
+            for i_method in methods:
+                results[i_url][i_method] = run_http_methods(i_url, i_method)
+
+
+    array_urls = create_set(array_urls)
+    array_urls = delete_items_there_are_not_urls(array_urls)
+    results = create_structure(array_urls)
+    main()
+    results = clean_structure(results)
+    create_and_write_to_file(results)
+    return results
+
+# time taken: 13.663326116045937
+'''
+if __name__ == '__main__':
+    args_control = {    "1",
+                        "https://google.com",
+                        "https://www.facebook.com"
+                    }
+    print(get_https_methods(args_control))
+'''
--- test_task.py
+++ test_task.py
@@ -1,7 +1,8 @@
 import os
 import task
-
+from time import perf_counter
 def test_get_info_hrefs():
+    start = perf_counter()
     args_control2 = [    '1',
                     'w3.com',
                     'http://w3.com',
@@ -19,15 +20,29 @@ def test_get_info_hrefs():
                     'www.google.com',
                     'linkedin.com/in/george-binder/',
                     'pythonexamples.org/',]
+    try:
+        os.remove('task.json')
+    except:
+        pass
+    assert task.get_https_methods(args_control2)
+    assert os.path.exists('task.json')
 
-    os.remove('task.json')
-    assert task.get_info_hrefs(args_control2) and os.path.exists('task.json')
+    stop = perf_counter()
+    print("time taken:", stop - start)
 
-    os.remove('task.json')
-    assert task.get_info_hrefs('hello') == ({})  and os.path.exists('task.json')
+    try:
+        os.remove('task.json')
+    except:
+        pass
+    assert task.get_https_methods('hello') == {}
+    assert os.path.exists('task.json')
 
-    os.remove('task.json')
-    assert task.get_info_hrefs(None) == ({})  and os.path.exists('task.json')
+    try:
+        os.remove('task.json')
+    except:
+        pass
+    assert task.get_https_methods(None) == {}
+    assert os.path.exists('task.json')
 
     args_control = {    "1",
                         "https://google.com",
@@ -44,5 +59,11 @@ def test_get_info_hrefs():
                                       'POST': 200,
                                       'PUT': 200}}
 
-    os.remove('task.json')
-    assert task.get_info_hrefs(args_control) == result_control and os.path.exists('task.json')
+    try:
+        os.remove('task.json')
+    except:
+        pass
+    assert task.get_https_methods(args_control) == result_control
+    assert os.path.exists('task.json')
+    print("time taken:", stop - start)
+#18.10932699299883
